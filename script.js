5eb32db41441d57f375cf287c0550e211d2c2d26
STarrant <stephenvtarrant@gmail.com>
2021-03-27 22:38:56 +0000

    Feat: Addition game start
    Random number function added.
    Errors to console created.
    Operators and operand for addition game linked.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function runGame() {
+// Event Handlers
 
+//Wait for the DOM to load before begining the game.
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+});
+
+
+// Functions
+function runGame(gameType) {
+    //Generate two random numbers:
+    let num1 = Math.floor(Math.random() * 25 ) + 1;
+    let num2 = Math.floor(Math.random() * 25 ) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown gametype ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -18,18 +50,20 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+}
+
+function displaySubtractQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
 function displayDivideQuestion() {
-    
+
 }
